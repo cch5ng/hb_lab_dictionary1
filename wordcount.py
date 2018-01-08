82e6c083d14db4051056d20808d8f9b49fe3f84d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 12:46:28 -0800

    solved part 1; added punctuation and case cleanup

--- wordcount.py
+++ wordcount.py
@@ -2,16 +2,38 @@
 def count_words(file_name):
     """Given a file, outputs a frequency count for each count (dictionary)."""
 
-
     file = open(file_name)
     word_count = {}
 
     for line in file:
-        tokens = line.rstrip().split(' ')
+        tokens = line.rstrip().split()
 
         for token in tokens:
-            add_word(word_count, token)
+            add_word(word_count, token.lower())
+
+    for key, value in word_count.iteritems():
+        print key, value
 
 
 def add_word(word_count, word):
     """Given a dictionary and word, increments the word count in the dictionary."""
+
+    clean_word = remove_punctuation(word)
+    word_count[clean_word] = word_count.get(clean_word, 0) + 1
+
+
+def remove_punctuation(word):
+    """Given a word, remove punctuation if it exists (expect at end)."""
+
+    punctuation = set([".", ",", ";", "!", "?", ":", "\"", "_", "*", "#"])
+    while len(word) > 0 and word[-1] in punctuation:
+        word = word[:-1]
+
+    while len(word) > 0 and word[0] in punctuation:
+        word = word[1:]
+
+    return word
+
+
+count_words('twain.txt')
+# count_words('test.txt')
